f2305a35f775efbcbf735c74ec3e1f724a2a0e2e
Michael Tsukerman <miketsukerman@gmail.com>
2020-04-03 17:59:05 +0200

    update conanfile.py to poperly package grammar library

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+import os
 
 class FpgConan(ConanFile):
     name = "fpg"
@@ -6,11 +7,23 @@ class FpgConan(ConanFile):
     license = "Apache2"
     author = "Michael Tsukerman <miketsukerman@gmail.com>"
     url = "https://github.com/miketsukerman/fpp-grammar"
-    description = "Franca plus parser"
+    description = "Franca plus grammar library"
     topics = ("bnfc", "franca", "parser", "genivi")
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     requires = "bison/3.3.2@bincrafters/stable", "flex/2.6.4@bincrafters/stable"
+    scm = {
+         "type": "git",
+         "subfolder": "fpp",
+         "url": "https://github.com/miketsukerman/fpp-grammar.git",
+         "revision": "master"
+    }
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+
+        cmake.configure(source_folder=self.scm["subfolder"])
+        return cmake
 
     def build(self):
         cmake = self.configure_cmake()
@@ -20,3 +33,9 @@ class FpgConan(ConanFile):
     def package(self):
         cmake = self.configure_cmake()
         cmake.install()
+        
+    def package_info(self):
+        self.cpp_info.name = self.name
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib"))
+        self.env_info.CMAKE_PREFIX_PATH.append(self.package_folder)        
